3c14b73b881c7f0c2e5870f3507226ac61e3e7e3
Adrian Reyes <reyes.siprian@gmail.com>
2022-09-19 03:08:53 -0400

    add: decrypt module

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,13 @@ const VOWELS = {
 
 function encrypt() {
     let text = document.querySelector('#text').value;
+
+    if(text == '' || text == null) {
+        document.querySelector('.results__nofound').style.display = 'block';
+        document.querySelector('.results__content').style.display = 'none';
+        return;
+    }
+
     text = text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g,"");
 
     textArray = text.split('');
@@ -22,8 +29,8 @@ function encrypt() {
     
     encryptedText = encryptedText.join('');
 
-    const RESULT = document.querySelector('#result__text');
-    RESULT.innerHTML = encryptedText;
+    let result = document.querySelector('#result__text');
+    result.innerHTML = encryptedText;
 
     document.querySelector('#text').value = '';
     
@@ -31,6 +38,34 @@ function encrypt() {
     document.querySelector('.results__nofound').style.display = 'none';
 }
 
+function decrypt() {
+    let text = document.querySelector('#text').value;
+
+    console.log(typeof text);
+
+    if(text == '' || text == null) {
+        document.querySelector('.results__nofound').style.display = 'block';
+        document.querySelector('.results__content').style.display = 'none';
+        return;
+    }
+
+    text = text.toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g,"");
+
+    for(let vowel in VOWELS) {
+        text = text.replaceAll(VOWELS[vowel], vowel);
+    }
+
+    let result = document.querySelector('#result__text');
+    result.innerHTML = text;
+
+    document.querySelector('#text').value = '';
+
+    document.querySelector('.results__content').style.display = 'block';
+    document.querySelector('.results__nofound').style.display = 'none';
+}
+
 const ENCRYPT_BTN = document.querySelector('#encrypt');
 ENCRYPT_BTN.addEventListener("click", encrypt );
 
+const DECRYPT_BTN = document.querySelector('#decrypt');
+DECRYPT_BTN.addEventListener("click", decrypt );
